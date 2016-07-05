48a11b1ef86c8ec5d77bb214f57adc342f54dfeb
yuqi98 <yuqi98@icloud.com>
2016-07-05 14:31:55 -0400

    I don't know what to say

--- DoNothing/ViewController.m
+++ DoNothing/ViewController.m
@@ -14,6 +14,8 @@
 
 @implementation ViewController
 
+@synthesize magicNumber;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
